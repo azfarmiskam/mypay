3d43a7f2829b33200a21ef7928248c943e20adc2
azfarmiskam <mohdazfar2401@gmail.com>
2025-11-25 07:22:11 +0800

    fix: Add MyPay favicon to SuperAdmin dashboard
    - Added favicon.png and favicon.ico links to admin layout
    - Favicon now displays in browser tab for SuperAdmin dashboard

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -7,6 +7,10 @@
 
     <title>@yield('title', 'Dashboard') - {{ config('app.name', 'MyPay') }} Admin</title>
 
+    <!-- Favicon -->
+    <link rel="icon" type="image/png" href="{{ asset('favicon.png') }}">
+    <link rel="shortcut icon" href="{{ asset('favicon.ico') }}">
+
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=inter:400,500,600,700&display=swap" rel="stylesheet" />
